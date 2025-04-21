4f9c504133995708589327635300fc9f345eb854
wesley <wesley93@ksu.edu>
2025-04-20 20:04:42 -0500

    Added the changes to Pthread.sh

--- hw4/3way-pthread/program_thread.sh
+++ hw4/3way-pthread/program_thread.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-./pthread_program ~dan/625/wiki_dump.txt
-./pthread_program ~dan/625/wiki_dump.txt
-./pthread_program ~dan/625/wiki_dump.txt
+./pthread_program ~dan/625/wiki_dump.txt $1
+./pthread_program ~dan/625/wiki_dump.txt $1
+./pthread_program ~dan/625/wiki_dump.txt $1
+./pthread_program ~dan/625/wiki_dump.txt $1
+./pthread_program ~dan/625/wiki_dump.txt $1
