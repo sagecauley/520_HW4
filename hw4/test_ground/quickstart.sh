8e945174e3012ac0ef11fc74d6b76a4dacc80925
wesley <wesley93@ksu.edu>
2025-05-04 13:22:47 -0500

    added a quick start. Might make it run the program

---
+++ hw4/test_ground/quickstart.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#This script is to qucickly set up the environment for running the programs 
+# I am lazy to do it manually
+
+chmod +x configs.sh
+chmod +x program_thread.sh
+chmod +x pthread_dif_files.sh
