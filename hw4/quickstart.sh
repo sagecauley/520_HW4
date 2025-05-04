4faf355c0dce7cc2efbbdd6593fcef3d5b9ca73b
wesley <wesley93@ksu.edu>
2025-05-04 13:38:06 -0500

    Made it do more like make the program. There is code to auto run the configurations but I am too scared to do it. PLEASE hold my hand

--- hw4/quickstart.sh
+++ hw4/quickstart.sh
@@ -7,11 +7,42 @@ chmod +x 3way-pthread/configs.sh
 chmod +x 3way-pthread/program_thread.sh
 chmod +x 3way-pthread/pthread_dif_files.sh
 
+#go into the 3way-pthread directory
+cd 3way-pthread
+#make the program
+#make clean
+make
+#run the configs.sh file
+#./configs.sh
+#exit
+#go back to the main directory
+cd ..
+
 chmod +x 3way-mpi/configs.sh
 chmod +x 3way-mpi/program_thread.sh
 chmod +x 3way-mpi/pthread_dif_files.sh
+#go into the 3way-mpi directory
+cd 3way-mpi
+#make the program
+#make clean
+make
+#run the configs.sh file
+#./configs.sh
+#exit
+#go back to the main directory
+cd ..
 
 chmod +x 3way-openmp/configs.sh
 chmod +x 3way-openmp/program_thread.sh
 chmod +x 3way-openmp/pthread_dif_files.sh
 
+#go into the 3way-openmp directory
+cd 3way-openmp
+#make the program
+#make clean
+make
+#run the configs.sh file
+#./configs.sh
+#exit
+#go back to the main directory
+cd ..
