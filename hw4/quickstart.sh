62e2362e62508459bda9b5a7271815dd99b86202
wesley <wesley93@ksu.edu>
2025-05-04 13:31:31 -0500

    Added the quickstart to make all of the folder executables

---
+++ hw4/quickstart.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#This script is to qucickly set up the environment for running the programs 
+# I am lazy to do it manually
+
+chmod +x 3way-pthread/configs.sh
+chmod +x 3way-pthread/program_thread.sh
+chmod +x 3way-pthread/pthread_dif_files.sh
+
+chmod +x 3way-mpi/configs.sh
+chmod +x 3way-mpi/program_thread.sh
+chmod +x 3way-mpi/pthread_dif_files.sh
+
+chmod +x 3way-openmp/configs.sh
+chmod +x 3way-openmp/program_thread.sh
+chmod +x 3way-openmp/pthread_dif_files.sh
+
--- hw4/test_ground/quickstart.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#This script is to qucickly set up the environment for running the programs 
-# I am lazy to do it manually
-
-chmod +x configs.sh
-chmod +x program_thread.sh
-chmod +x pthread_dif_files.sh
