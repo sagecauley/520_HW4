6b401dd826bf74a1f06fcc9f533ba71fda27e587
wesley <wesley93@ksu.edu>
2025-05-04 16:27:02 -0500

    added things

--- hw4/3way-mpi/program_thread.sh
+++ hw4/3way-mpi/program_thread.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "I AM $1 Number of cores"
 echo "I am using dan/625/wiki_dump.txt for input"
-perf stat mpirun -np $1 ./mpi_program ~dan/625/wiki_dump.txt $1
-perf stat mpirun -np $1 ./mpi_program ~dan/625/wiki_dump.txt $1
-perf stat mpirun -np $1 ./mpi_program ~dan/625/wiki_dump.txt $1
-perf stat mpirun -np $1 ./mpi_program ~dan/625/wiki_dump.txt $1
-perf stat mpirun -np $1 ./mpi_program ~dan/625/wiki_dump.txt $1
+perf stat mpirun --use-hwthread-cpus ./mpi_program ~dan/625/wiki_dump.txt $1
+#perf stat mpirun --use-hwthread-cpus ./mpi_program ~dan/625/wiki_dump.txt $1
+#perf stat mpirun --use-hwthread-cpus ./mpi_program ~dan/625/wiki_dump.txt $1
+#perf stat mpirun --use-hwthread-cpus ./mpi_program ~dan/625/wiki_dump.txt $1
+#perf stat mpirun --use-hwthread-cpus ./mpi_program ~dan/625/wiki_dump.txt $1
