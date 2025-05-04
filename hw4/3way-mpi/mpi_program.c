563e3f4dacb323d80059c954078ececee787a613
sagecauley <sagecauley@ksu.edu>
2025-05-04 16:58:41 -0500

    Now maybe??

--- hw4/3way-mpi/mpi_program.c
+++ hw4/3way-mpi/mpi_program.c
@@ -1,6 +1,7 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 #define MAX_LINE_LENGTH 1024
@@ -65,69 +66,91 @@ int main(int argc, char* argv[]) {
                 lines = new_lines;
             }
 
-            // Copy raw memory instead of using strncpy
-            for (int i = 0; i < MAX_LINE_LENGTH; ++i) {
-                lines[num_lines][i] = buffer[i];
-                if (buffer[i] == '\0') break;
-            }
+            strncpy(lines[num_lines], buffer, MAX_LINE_LENGTH);
             num_lines++;
         }
 
         fclose(file);
     }
 
-    // Broadcast number of lines
+    // Broadcast number of lines to all processes
     MPI_Bcast(&num_lines, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Compute send counts and displacements
-    int* sendcounts = malloc(num_procs * sizeof(int));
-    int* displs = malloc(num_procs * sizeof(int));
+    // Compute send counts and displacements (in bytes)
+    int* sendcounts = NULL;
+    int* displs = NULL;
 
-    int base = num_lines / num_procs;
-    int rem = num_lines % num_procs;
+    if (pid == 0) {
+        sendcounts = malloc(num_procs * sizeof(int));
+        displs = malloc(num_procs * sizeof(int));
 
-    for (int i = 0; i < num_procs; ++i) {
-        sendcounts[i] = (i < rem ? base + 1 : base) * MAX_LINE_LENGTH;
-        displs[i] = (i == 0) ? 0 : displs[i-1] + sendcounts[i-1];
+        int base = num_lines / num_procs;
+        int rem = num_lines % num_procs;
+
+        for (int i = 0; i < num_procs; ++i) {
+            int count = (i < rem ? base + 1 : base);
+            sendcounts[i] = count * MAX_LINE_LENGTH;
+            displs[i] = (i == 0) ? 0 : displs[i - 1] + sendcounts[i - 1];
+        }
     }
 
-    int local_line_count = sendcounts[pid] / MAX_LINE_LENGTH;
-    char* local_lines = malloc(sendcounts[pid]);
+    // Each process calculates how many lines it will get
+    int local_line_bytes;
+    MPI_Scatter(sendcounts, 1, MPI_INT, &local_line_bytes, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    int local_line_count = local_line_bytes / MAX_LINE_LENGTH;
+
+    char* local_lines = malloc(local_line_bytes);
     int* local_max_ascii = malloc(local_line_count * sizeof(int));
 
-    // Scatter data
-    MPI_Scatterv(lines, sendcounts, displs, MPI_CHAR,
-             local_lines, sendcounts[pid], MPI_CHAR,
-             0, MPI_COMM_WORLD);
+    // Scatterv lines to all processes
+    MPI_Scatterv(pid == 0 ? lines : NULL, sendcounts, displs, MPI_CHAR,
+                 local_lines, local_line_bytes, MPI_CHAR,
+                 0, MPI_COMM_WORLD);
 
     // Compute local max ASCII values
     for (int i = 0; i < local_line_count; ++i) {
         local_max_ascii[i] = max_ascii_value_mpi(&local_lines[i * MAX_LINE_LENGTH]);
     }
 
-    int* recvcounts = sendcounts;
-    int* recvdispls = displs;
+    // Compute recvcounts and displs for gathering INTs (not bytes)
+    int* recvcounts_int = NULL;
+    int* displs_int = NULL;
+    if (pid == 0) {
+        recvcounts_int = malloc(num_procs * sizeof(int));
+        displs_int = malloc(num_procs * sizeof(int));
+        for (int i = 0; i < num_procs; ++i) {
+            recvcounts_int[i] = sendcounts[i] / MAX_LINE_LENGTH;
+            displs_int[i] = (i == 0) ? 0 : displs_int[i - 1] + recvcounts_int[i - 1];
+        }
+    }
 
     int* all_max_ascii = NULL;
     if (pid == 0) {
         all_max_ascii = malloc(num_lines * sizeof(int));
     }
 
+    // Gather all max ASCII values to root
     MPI_Gatherv(local_max_ascii, local_line_count, MPI_INT,
-            all_max_ascii, recvcounts, recvdispls, MPI_INT,
-            0, MPI_COMM_WORLD);
+                all_max_ascii, recvcounts_int, displs_int, MPI_INT,
+                0, MPI_COMM_WORLD);
 
     if (pid == 0) {
         for (int i = 0; i < num_lines; ++i) {
-            //printf("%d: %d\n", i, all_max_ascii[i]);
+            printf("Line %d max ASCII: %d\n", i, all_max_ascii[i]);
         }
 
         free(all_max_ascii);
+        free(lines);
+        free(sendcounts);
+        free(displs);
+        free(recvcounts_int);
+        free(displs_int);
     }
 
-    // Free allocated memory
     free(local_lines);
     free(local_max_ascii);
-    free(sendcounts);
-    free(displs);
+
+    MPI_Finalize();
+
+    return 0;
 }
