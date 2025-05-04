678f8c199f6b6fe760747f4ebe471f6a10a02b0d
wesley <wesley93@ksu.edu>
2025-05-04 13:09:45 -0500

    STAT?

--- hw4/test_ground/program_thread.sh
+++ hw4/test_ground/program_thread.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-perf report --stdio ./pthread_program ~dan/625/wiki_dump.txt $1
-perf report --stdio ./pthread_program ~dan/625/wiki_dump.txt $1
-perf report --stdio ./pthread_program ~dan/625/wiki_dump.txt $1
-perf report --stdio ./pthread_program ~dan/625/wiki_dump.txt $1
-perf report --stdio ./pthread_program ~dan/625/wiki_dump.txt $1
+perf stat ./pthread_program ~dan/625/wiki_dump.txt $1
+perf stat ./pthread_program ~dan/625/wiki_dump.txt $1
+perf stat ./pthread_program ~dan/625/wiki_dump.txt $1
+perf stat ./pthread_program ~dan/625/wiki_dump.txt $1
+perf stat ./pthread_program ~dan/625/wiki_dump.txt $1
